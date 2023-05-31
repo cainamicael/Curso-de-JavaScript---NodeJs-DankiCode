2b9873883e4fe01e16333584af5727d96f149974
cainamicael <cainamicaeloficial@gmail.com>
2023-05-31 10:51:41 -0300

    add

--- Curso node.js/Aula 01/index.js
+++ Curso node.js/Aula 01/index.js
@@ -1,5 +1,19 @@
-function somar(n1, n2) {
-    console.log(n1 + n2)
-}
+//Cabeçalho do servidor
+const http = require('http') //Vamos importar do módulo
 
-setInterval(() => {somar(10, 20)}, 2000)
+const hostName = '127.0.0.1' //Localhost
+const port = 3000
+
+//Montando o servidor
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200//Resposta para quem está acessando o server
+    res.setHeader('Content-Type', 'text/plain')
+
+    res.end('Hello World')//O que vai mostrar quando o user acessar
+
+})
+
+server.listen(port, hostName, () => {
+    console.log('Servidor está rodando')
+    
+})
