c73131f7369b6e8eb6516581746b64222f0000c6
cainamicael <cainamicaeloficial@gmail.com>
2023-05-31 11:15:03 -0300

    add

--- Curso node.js/Aula 01/index.js
+++ Curso node.js/Aula 01/index.js
@@ -1,19 +1,30 @@
-//Cabeçalho do servidor
-const http = require('http') //Vamos importar do módulo
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-const hostName = '127.0.0.1' //Localhost
+const hostName = '127.0.0.1'
 const port = 3000
 
-//Montando o servidor
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200//Resposta para quem está acessando o server
-    res.setHeader('Content-Type', 'text/plain')
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html')
 
-    res.end('Hello World')//O que vai mostrar quando o user acessar
+    //O req mostra todas as informações da requisição
+    //Trabalhando com endpoints
+    if(req.url == '/danki' ) {
+        //Abrindo um arquivo html
+        fs.readFile(filePath, (error, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data) //É o próprio arquivo html, no caso, é o prório filePath
+        res.end()
 
+        })
+
+    } else {
+        return res.end()//Sem o endpoint, retorna vazio - Se não tiver o else, carrega infinitamete
+    }
+  
 })
 
 server.listen(port, hostName, () => {
-    console.log('Servidor está rodando')
-    
+  console.log('Servidor está rodando')
 })
