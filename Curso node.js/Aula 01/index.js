c92a2098505625b7097277bf4a423ec2976139a4
cainamicael <cainamicaeloficial@gmail.com>
2023-05-31 14:23:23 -0300

    add

--- Curso node.js/Aula 01/index.js
+++ Curso node.js/Aula 01/index.js
@@ -4,6 +4,12 @@ const path = require('path')
 const pathFile = path.join(__dirname, 'danki.txt')
 
 fs.readFile(pathFile, (err, data) => {
-    console.log(data.toString())//Para ficar visível ao invés de ficar em buffer
+    let str = data.toString()
+
+    let newStr = str.split('/')//Cria um array
+    newStr.forEach(nome => console.log(nome))
+
+    let newStr2 = str.substring(0, 3)//Pega os caracteres desde o indice do primeiro param. atá o indice do 2º param.
+    console.log(newStr2)
 
 })
