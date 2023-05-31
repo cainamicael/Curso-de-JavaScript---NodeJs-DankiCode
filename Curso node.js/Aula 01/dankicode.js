cebb109f6244c42f25a014c563b4c514bc83c6fc
cainamicael <cainamicaeloficial@gmail.com>
2023-05-31 15:10:42 -0300

    add

--- Curso node.js/Aula 01/dankicode.js
+++ Curso node.js/Aula 01/dankicode.js
@@ -2,4 +2,11 @@ function helloDankiCode() {
     console.log('Olá mundo!')
 }
 
+var n = 10
+
+var obj = {nome: 'Guilherme', idade: 27}
+
+//Configurando saída e nomes
 exports.helloDankiCode = helloDankiCode
+exports.num = n;//Posso colocar o nome que eu quiser
+exports.pessoa = obj
--- Curso node.js/Aula 01/index.js
+++ Curso node.js/Aula 01/index.js
@@ -2,3 +2,5 @@
 const dankicode = require('./dankicode.js')
 
 dankicode.helloDankiCode()
+console.log(dankicode.num)
+console.log(dankicode.pessoa.nome)
