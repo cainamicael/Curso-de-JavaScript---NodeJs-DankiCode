d78db849bef61fd5b24548fae74b9a145b0c0df2
cainamicael <cainamicaeloficial@gmail.com>
2023-06-03 18:07:29 -0300

    add

--- Curso node.js/Sockets/index.js
+++ Curso node.js/Sockets/index.js
@@ -9,17 +9,9 @@ app.get('/', (req, res) => {
 
 //Para sabermos quando o usuário for conectado
 io.on('connection', socket => { //O socket faz referência ao usuário
-    console.log('Usuário conectado ' + socket.id)//A gente pode ver detalhes da conexão
-
-    //Vai enviar o dado para o frontend - Vai enviar para todos os usuários da aplicação
-    io.emit('conectado', 'Aqui é o conteúdo que vamos passar para o front')//no front tem que ser o mesmo nome
-
-    //Para enviar um dado para todos os usuários, com excessão do que entrou agora
-    socket.broadcast.emit('novo usuario', `Usuário ${socket.id} acabou de entrar!`)//novo evento
-
-    //Já que o usuário é o parâmetro socket, para sabermos se ele está desconectado usamos:
-    socket.on('disconnect', () => {
-        console.log('Usuário desconectado')
+   
+    socket.on('chat message', (obj) => {
+        console.log(obj)
         
     })
 
