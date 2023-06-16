40b13dc9bf2c9985d3f25f2223b1abadb47f018a
cainamicael <cainamicaeloficial@gmail.com>
2023-06-15 23:14:50 -0300

    add

--- Curso node.js/Api RESTful/routes/personRoutes.js
+++ Curso node.js/Api RESTful/routes/personRoutes.js
@@ -6,9 +6,7 @@ router.post('/', async (req, res) => { //Tiro o person, pois já foi setado no i
   const {name, salary, approved} = req.body
 
   //Mini verificação. O ideal é ter  uma verificação para cada campo
-  if(!name) {
-      res.status(422).json({erro: "O nome é obrigatório"})
-  }
+  if(!name) {return res.status(422).json({erro: "O nome é obrigatório"})}
 
   const person = {name, salary, approved}
 
@@ -38,10 +36,15 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id
 
     const person = await Person.findOne({_id: id})
+
+    if(!person) {return res.status(500).json({message: "O usuário não foi encontrado"})}
+
     res.status(200).json(person)
   } catch (e) {
     res.status(500).json({erro: e})
   }
 })
+
+//Update (put = Objeto completo, patch = Atualização parcial)
 
 module.exports = router
