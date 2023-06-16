1bfda707f5019a6fff240334bbd4250aa8e4d0ff
cainamicael <cainamicaeloficial@gmail.com>
2023-06-15 22:25:15 -0300

    add

--- Curso node.js/Api RESTful/index.js
+++ Curso node.js/Api RESTful/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv/config')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT
 
 const app = express()
@@ -8,8 +9,16 @@ const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
-
-app.listen(PORT, () => {
-  console.log('Server rodando na porta ' + PORT)
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Oi Express'})
 })
+
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.SECRET_BD}@cluster0.dmxkhyz.mongodb.net/${process.env.NOME_BD}?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log('Conectamos ao MongoDB')
+
+    app.listen(PORT, () => {
+      console.log('Server rodando na porta ' + PORT)
+    })
+  })
+  .catch(e => console.log(e.message))
