b6cb24884dd30fb635e48aa5d8a3aad13bbcf739
cainamicael <cainamicaeloficial@gmail.com>
2023-06-17 21:48:16 -0300

    add

--- Curso node.js/API Restful com AdonisJS/app/Controllers/Http/MomentsController.ts
+++ Curso node.js/API Restful com AdonisJS/app/Controllers/Http/MomentsController.ts
@@ -5,8 +5,13 @@ export default class MomentsController {
     public async store({request, response}: HttpContextContract) {
         const body = request.body()
 
+        const moment = await Moment.create(body)//Fará o insert no bd
+
+        response.status(201)
+
         return{
-            body,
+            msg: "Momento criado com sucesso",
+            data: moment
         }
     }
    
