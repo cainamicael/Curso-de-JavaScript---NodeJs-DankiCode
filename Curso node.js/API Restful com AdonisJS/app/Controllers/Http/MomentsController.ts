c61a0611c584a6bc9634bacb3f1d92a1e07d9698
cainamicael <cainamicaeloficial@gmail.com>
2023-06-17 22:06:56 -0300

    add

--- Curso node.js/API Restful com AdonisJS/app/Controllers/Http/MomentsController.ts
+++ Curso node.js/API Restful com AdonisJS/app/Controllers/Http/MomentsController.ts
@@ -1,9 +1,27 @@
+import {v4 as uuidv4} from 'uuid'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Moment from 'App/Models/Moment'
+import Application from '@ioc:Adonis/Core/Application' //para upload
 
 export default class MomentsController {
+    private validationOptions = {
+        types: ['image'],
+        size:  '2mb'
+    }
+
     public async store({request, response}: HttpContextContract) {
         const body = request.body()
+        const image = request.file('image', this.validationOptions)//image é o nome do input
+
+        if(image){
+            const imageName = `${uuidv4()}.${image.extname}`
+
+            await image.move(Application.tmpPath('uploads', {
+                name: imageName
+            }))//Onde quero que fiquue guardada no meu server
+
+            body.image = imageName
+        }
 
         const moment = await Moment.create(body)//Fará o insert no bd
 
