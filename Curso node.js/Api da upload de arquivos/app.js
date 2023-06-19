d5ca05a6857c5f0f43b79697d11d57063dda8876
cainamicael <cainamicaeloficial@gmail.com>
2023-06-19 18:20:48 -0300

    add

---
+++ Curso node.js/Api da upload de arquivos/app.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log('Server rodando na porta ' + PORT)
+})
